d5be7a82741a14566638c679c16e672863a3f378
Perc2val <fsb099@aol.de>
2026-01-08 23:07:16 +0100

    js can create a card but innerhtml is wrong

--- javascript.js
+++ javascript.js
@@ -1,18 +1,50 @@
-function Book(title, author, pages, read) {
-    if (!new.target) {
-        throw Error("function call has to contain new");
-    }
+const myLibrary = [];
+const hello = document.getElementById("test")
 
+function Book(title, author, pages, read) {
+    if (!new.target){
+        throw Error ("function call has to coontain new");
+    }
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-
-    this.info = function () {
-        const readStatus = this.read ? "already read" : "not read yet";
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
-    };
+    this.id = crypto.randomUUID();
 }
 
-let theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295 , true)
-let percyJackson = new Book("Percy Jackson", "Rick Rijordan", 360, true)
+function addBookToLibrary(title) {
+    title = prompt("Type in the title");
+    author = prompt("Type in the author");
+    pages = prompt("How many pages does the book have?")
+    read = prompt("have you read the book")
+
+    let newBook = new Book(title, author, pages, read)
+  // take params, create a book then store it in the array
+    myLibrary.push(newBook);
+}
+addBookToLibrary()
+addBookToLibrary()
+
+
+function displayMyLibrary(){
+    for (newBook in myLibrary) {
+        const newCard = document.createElement("div");
+        const heading = document.createElement("h2");
+        heading.innerHTML = `${myLibrary[0].title}`;
+        const author = document.createElement("p");
+        const pages = document.createElement("p");
+        const read = document.createElement("input");
+        const label = document.createElement("label");
+
+        
+        newCard.appendChild(heading);
+        newCard.appendChild(author);
+        newCard.appendChild(pages);
+        newCard.appendChild(read);
+        newCard.appendChild(label);
+        hello.appendChild(newCard);
+        
+    }
+}
+
+displayMyLibrary()
